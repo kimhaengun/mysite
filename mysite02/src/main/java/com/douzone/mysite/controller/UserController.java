6e56681f9a7de271082716f19351d50ceec59b8a
kimhaengun <chxl89@naver.com>
2021-10-12 13:50:26 +0900

    action & actionFactory

--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.douzone.mysite.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.douzone.web.util.MvcUtil;
+import com.douzone.mysite.mvc.UserActionFactory;
+import com.douzone.web.mvc.Action;
+import com.douzone.web.mvc.ActionFactory;
 
 //@WebServlet("/UserController")
 public class UserController extends HttpServlet {
@@ -16,17 +19,21 @@ public class UserController extends HttpServlet {
 		// TODO Auto-generated method stub
 		request.setCharacterEncoding("UTF-8");
 		
-		String action = request.getParameter("cmd");
-		if("joinform".equals(action)) {
-			MvcUtil.forward("WEB-INF/views/user/joinform.jsp", request, response);		
-		}else if("join".equals(action)) {
-			
-		}else {
-			//장난친놈 - 비정상 주소
-			MvcUtil.redirect("/mysite02",request,response);
-		}
+		String actionName = request.getParameter("cmd");
+
+		ActionFactory af = new UserActionFactory();
+		Action action = af.getAction(actionName);
+		action.execute(request,response);
+
+//		if("joinform".equals(action)) {
+//			MvcUtil.forward("WEB-INF/views/user/joinform.jsp", request, response);		
+//		}else if("join".equals(action)) {
+//			
+//		}else {
+//			//장난친놈 - 비정상 주소
+//			MvcUtil.redirect("/mysite02",request,response);
+//		}
 		
-	
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
---
+++ mysite02/src/main/java/com/douzone/mysite/mvc/UserActionFactory.java
@@ -0,0 +1,22 @@
+package com.douzone.mysite.mvc;
+
+import com.douzone.web.mvc.Action;
+import com.douzone.web.mvc.ActionFactory;
+
+public class UserActionFactory extends ActionFactory {
+
+	@Override
+	public Action getAction(String actionName) {
+		// TODO Auto-generated method stub
+		Action action = null;
+		if("joinform".equals(actionName)) {
+//			action = new JoinFormAction();
+			
+		}else {
+			
+		}
+		
+		return action;
+	}
+
+}
