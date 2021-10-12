49fb8621a979d1ea2b3675c68e5f819ca0c3fede
kimhaengun <chxl89@naver.com>
2021-10-12 12:18:36 +0900

    user service #1

---
+++ docs/mysitesql.sql
@@ -0,0 +1,2 @@
+use webdb;
+desc user;
--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -1,8 +1,6 @@
 package com.douzone.mysite.controller;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,20 +8,26 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.douzone.web.util.MvcUtil;
 
-
-//@WebServlet("/main")
-public class MainController extends HttpServlet {
+//@WebServlet("/UserController")
+public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		request.setCharacterEncoding("UTF-8");
 		
-		MvcUtil.forward("WEB-INF/views/main/index.jsp", request, response);
-
+		String action = request.getParameter("cmd");
+		if("joinform".equals(action)) {
+			MvcUtil.forward("WEB-INF/views/user/joinform.jsp", request, response);		
+		}else if("join".equals(action)) {
+			
+		}else {
+			//장난친놈 - 비정상 주소
+			MvcUtil.redirect("/mysite02",request,response);
+		}
+		
+	
 	}
-
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
