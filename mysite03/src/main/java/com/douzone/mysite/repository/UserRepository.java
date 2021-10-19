6ace92f52da10f13e326c690fd089a1667be44b4
kimhaengun <chxl89@naver.com>
2021-10-19 15:59:17 +0900

    mysite03 익셉션 페이지 생성

--- mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
@@ -144,7 +144,7 @@ public class UserRepository {
 	}
 	
 	
-	public UserVo findByEmailAndPassword(String email,String password) {
+	public UserVo findByEmailAndPassword(String email,String password) throws UserRepositoryExcetion {
 		// TODO Auto-generated method stub
 		UserVo vo = null;
 		Connection conn = null;
@@ -153,7 +153,7 @@ public class UserRepository {
 		try {
 			conn = getConnection();
 			//3. SQL 준비
-			String sql = "select no,name from user where email = ? and password = ?";
+			String sql = "selects no,name from user where email = ? and password = ?";
 			pstmt = conn.prepareStatement(sql);
 			
 			pstmt.setString(1, email);
@@ -172,7 +172,7 @@ public class UserRepository {
 			}
 
 		}catch(SQLException e) {
-			System.out.println("error : "+e);
+			throw new UserRepositoryExcetion(e.toString());
 		}finally {
 			try {
 				if(rs != null) {
