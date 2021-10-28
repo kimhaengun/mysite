7cefa2a33f78de4b00a0a144fd27542b7f11e186
kimhaengun <chxl89@naver.com>
2021-10-28 13:39:02 +0900

    Appconfig (DBConfig + MybatisConfig)

--- mysite04/src/main/java/com/douzone/config/app/MyBatisConfig.java
+++ mysite04/src/main/java/com/douzone/config/app/MyBatisConfig.java
@@ -2,8 +2,10 @@ package com.douzone.config.app;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,5 +23,10 @@ public class MyBatisConfig {
 		
 		
 		return sqlSessionFactoryBean.getObject();
+	}
+	
+	@Bean
+	public SqlSession sqlSession(SqlSessionFactory sqlSessionFactory) {
+		return new SqlSessionTemplate(sqlSessionFactory);
 	}
 }
